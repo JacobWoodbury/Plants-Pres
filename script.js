00d1c25052cb7361f011b00d2cc01325d728da27
JACOB_WOODBURY <166659344+JacobWoodbury@users.noreply.github.com>
2025-02-02 20:35:56 -0800

    new stuff

--- script.js
+++ script.js
@@ -4,16 +4,14 @@ document.getElementById("next").onclick = function(){nextSlide();}
 let container = document.getElementById("slide-container");
 let voice = document.getElementById("voice");
 let slide = document.createElement("img");
-let imgOne = "one.png";
-let imgTwo = "two.png";
-let imgThree = "three.png";
-let collection = [imgOne,imgTwo,imgThree];
-let audioCollection = ["voiceOne.mp3", "voiceTwo.mp3", "voiceThree.mp3"]
+let clearMe = document.getElementById("clearMe");
+let collection = ["titleSlide.png","one.png", "two.png", "three.png", "four.png", "five.png", "six.png", "seven.png", "biblio.png" ];
+let audioCollection = ["introAudio.mp3","one.mp3", "two.mp3", "three.mp3", "four.mp3", "five.mp3", "six.mp3", "seven.mp3"]
 let currentSlide = -1;
 
 
 function nextSlide(){
-
+clearMe.innerHTML = "";
 if(currentSlide >= collection.length -1){
     currentSlide = 0;
 }else{
