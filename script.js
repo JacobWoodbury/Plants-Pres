5cac6189ad0e9966fe0af7347541e6353d1b160f
JACOB_WOODBURY <166659344+JacobWoodbury@users.noreply.github.com>
2025-02-02 20:39:49 -0800

    test

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let clearMe = document.getElementById("clearMe");
 let collection = ["titleSlide.png","one.png", "two.png", "three.png", "four.png", "five.png", "six.png", "seven.png", "biblio.png" ];
 let audioCollection = ["introAudio.mp3","one.mp3", "two.mp3", "three.mp3", "four.mp3", "five.mp3", "six.mp3", "seven.mp3"]
 let currentSlide = -1;
-
+//ok
 
 function nextSlide(){
 clearMe.innerHTML = "";
